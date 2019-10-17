b4f7621a35d67f789e10f7641a409f9709fb7720
yuiseki <yuiseki@gmail.com>
2019-10-18 06:19:40 +0900

    add test file for Twitter class

---
+++ functions/test/twitter.ts
@@ -0,0 +1,12 @@
+import { describe, it } from 'mocha'
+import assert = require('assert')
+import { Twitter } from '../src/twitter'
+
+describe('Twitterクラスのテスト', () => {
+
+  it('new Twitter()できる', async () => {
+    const twitter = new Twitter()
+    assert(twitter!==null)
+  })
+
+})
